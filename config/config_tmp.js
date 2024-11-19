1115de82e3ef31b0b115b3370be68fb11696d1e5
D4rkCipherX <185471808+Not-D4rkCipherX@users.noreply.github.com>
2024-11-19 23:09:02 +0600

    Update config_tmp.js

--- config/config_tmp.js
+++ config/config_tmp.js
@@ -1,6 +1,6 @@
 export class Config {
-  static TELEGRAM_APP_ID =27411753; // YOUR APP ID EX : 324893724923
-  static TELEGRAM_APP_HASH =2dc50cf9ae11326baa296eddd1de5d2c; // YOUR APP HASH EX: "aslkfjkalsjflkasf"
+  static TELEGRAM_APP_ID = undefined; // YOUR APP ID EX : 324893724923
+  static TELEGRAM_APP_HASH = undefined; // YOUR APP HASH EX: "aslkfjkalsjflkasf"
   static USEONCHAINTX = false; // SET To TRUE IF YOU WANT TO USE ONCHAIN TX FOR CHECK IN AND UPGRADE
   static USERWALLET = [
     [
